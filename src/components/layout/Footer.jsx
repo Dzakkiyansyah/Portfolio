0cbd026dc4643e3dec286517f4b5d3a7cbb21ab7
Dzakkiyansyah <dzakkiyansyah@gmail.com>
2025-06-08 01:29:51 +0800

    Perbaikan

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,10 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-8 border-t border-gray-200 bg-light-background dark:bg-dark-background dark:border-gray-800">
       <div className="container px-6 mx-auto text-center text-subtle dark:text-subtle-dark">
-        <p>
-          &copy; {currentYear} Dzakkiyansyah. Dibuat dengan{' '}
-          <span className="text-primary">&hearts;</span> dan React.
-        </p>
+        <p>&copy; {currentYear} Dzakkiyansyah.</p>
       </div>
     </footer>
   );
